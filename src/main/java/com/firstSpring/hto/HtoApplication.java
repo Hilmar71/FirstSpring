b78f45bf2ad38f63326471c72f7bf1d2654049d9
Hilmar71 <hilmartho@gmail.com>
2018-09-06 11:47:10 +0200

    Commit HW

---
+++ src/main/java/com/firstSpring/hto/HtoApplication.java
@@ -0,0 +1,12 @@
+package com.firstSpring.hto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/firstSpring/hto/HtoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.firstSpring.hto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HtoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
